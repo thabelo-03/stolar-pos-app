5ba6eb03a16990f5e37b86bd6c6bae43ce13d176
Thabelo Dumani <thabstay@gmail.com>
2026-01-15 13:20:37 +0200

    Merged remote changes and finalized tabs

---
+++ app/config.ts
@@ -0,0 +1,9 @@
+import Constants from 'expo-constants';
+
+const debuggerHost = Constants.expoConfig?.hostUri;
+const localhost = debuggerHost?.split(':')[0];
+
+const SERVER_IP = localhost || '192.168.3.64';
+const PORT = '5000';
+
+export const API_BASE_URL = `http://${SERVER_IP}:${PORT}/api`;
