21274c9f5555855c2304fbc12a51c050cae063fa
fortuneM <fortuemoney@gmail.com>
2026-01-15 12:23:00 +0200

    global variable

--- app/(tabs)/api.ts
+++ app/(tabs)/api.ts
@@ -1,5 +1,5 @@
 // Replace this IP address with your computer's local IP
 const SERVER_IP = '192.168.3.64';
 const PORT = '5000';
-
+//global
 export const API_BASE_URL = `http://${SERVER_IP}:${PORT}/api`;
